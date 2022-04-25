f7e000d6aaba8ccd5681a5952afd62d860302244
Lennart Kuell <lennart.kuell@gmail.com>
2022-04-25 11:18:47 +0200

    Remove duplicate files #minor

--- src/Categories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './App.css';
-
-export default function Categories() {
-  return <div className="selection">Select category:</div>;
-}
--- src/Sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-function Sidebar() {
-  return <div className="sidebar">Filter</div>;
-}
-
-export default Sidebar;
